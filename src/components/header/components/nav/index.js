863b034ca6a806c12a9641b8e8fa54fbc3c5e030
r.polunin@buro247.ru <es54P25eseg74>
2021-11-21 23:37:11 +0300

    Pure react

--- src/components/header/components/nav/index.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-const Index = () => (
-	<nav>
-		<Link href="/">Главная</Link> <Link href="/articles">Статьи</Link> <Link href="/users">Пользователи</Link>
-	</nav>
-)
-
-export default Index;
--- src/components/header/index.js
+++
@@ -1,9 +0,0 @@
-import Nav from './components/nav/index';
-
-const Index = () => (
-	<header className="header">
-		<Nav/>
-	</header>
-);
-
-export default Index;
