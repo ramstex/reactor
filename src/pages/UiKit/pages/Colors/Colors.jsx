041bc7480a2f9a64e5f79296e0060020dc1f70da
r.polunin@buro247.ru <es54P25eseg74>
2022-04-13 18:56:10 +0300

    Colors ui-kit page

--- src/pages/UiKit/pages/Colors/Colors.jsx
+++ src/pages/UiKit/pages/Colors/Colors.jsx
@@ -28,21 +28,53 @@ class PageUiKitColors extends React.Component {
 
 					<Row>
 						<Col className={ this.classNameCol() } col={ 2 }>
-							<LocalColor code={ '#c0baff' } title={ 'primary' } />
+							<LocalColor code={ '#c0baff' } title={ 'Primary' } />
 						</Col>
 
 						<Col className={ this.classNameCol() } col={ 2 }>
-							<LocalColor code={ '#491818' } title={ 'secondary' } />
+							<LocalColor code={ '#491818' } title={ 'Secondary' } />
 						</Col>
 					</Row>
 
 					<Row>
 						<Col className={ this.classNameCol() } col={ 2 }>
-							<LocalColor code={ '#161616' } title={ 'text' } />
+							<LocalColor code={ '#30CF60' } title={ 'Success' } />
 						</Col>
 
 						<Col className={ this.classNameCol() } col={ 2 }>
-							<LocalColor code={ '#f5f5f5' } title={ 'back' } border={ '#161616' } />
+							<LocalColor code={ '#F7D264' } title={ 'Warning' } />
+						</Col>
+
+						<Col className={ this.classNameCol() } col={ 2 }>
+							<LocalColor code={ '#F77F6E' } title={ 'Danger' } />
+						</Col>
+
+						<Col className={ this.classNameCol() } col={ 2 }>
+							<LocalColor code={ '#ABDDED' } title={ 'Info' } />
+						</Col>
+					</Row>
+
+					<Row>
+						<Col className={ this.classNameCol() } col={ 2 }>
+							<LocalColor code={ '#D9D9D9' } title={ 'Muted' } />
+						</Col>
+
+						<Col className={ this.classNameCol() } col={ 2 }>
+							<LocalColor code={ '#999999' } title={ 'Muted dark' } />
+						</Col>
+
+						<Col className={ this.classNameCol() } col={ 2 }>
+							<LocalColor code={ '#EDEDED' } title={ 'Muted light' } />
+						</Col>
+					</Row>
+
+					<Row>
+						<Col className={ this.classNameCol() } col={ 2 }>
+							<LocalColor code={ '#161616' } title={ 'Text' } />
+						</Col>
+
+						<Col className={ this.classNameCol() } col={ 2 }>
+							<LocalColor code={ '#f5f5f5' } title={ 'Back' } border={ '#161616' } />
 						</Col>
 					</Row>
 				</MarkupSection>
