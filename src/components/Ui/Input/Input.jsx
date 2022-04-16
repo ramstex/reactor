d756f1e85755cc75afc76a7d4ec7afcc065b7c85
r.polunin@buro247.ru <es54P25eseg74>
2022-04-16 16:05:12 +0300

    Input component

--- src/components/Ui/Input/Input.jsx
+++ src/components/Ui/Input/Input.jsx
@@ -8,45 +8,91 @@ import './Input.scss';
 class UiInput extends React.Component {
 	//	Классы
 	classNameRoot() {
-		const { className, textarea } = this.props;
+		const { className, error, success, required, disabled, textarea } = this.props;
 		return classnames(
 			'ui-input',
 			className,
+			{ _required: required },
+			{ _disabled: disabled },
 			{ _textarea: textarea },
+			{ _error: error },
+			{ _success: success },
 		);
+	}
+
+	classNameCaption() {
+		return classnames( 'ui-input__caption' );
 	}
 
 	classNameInput() {
 		return classnames( 'ui-input__input', );
 	}
 
+	classNameMessage() {
+		return classnames( 'ui-input__message', );
+	}
+
 	render() {
-		const { textarea, type, placeholder } = this.props;
+		const { children, required, disabled, textarea, type, message, placeholder } = this.props;
 
 		const attrs = omit( this.props, [
 			'className',
 			'children',
+			'required',
 			'textarea',
+			'disabled',
+			'error',
+			'success',
+			'message',
 			'placeholder',
 			'type',
 		] );
 
 		return (
 			<div className={ this.classNameRoot() }>
-				{ textarea ?
-					<textarea className={ this.classNameInput() } placeholder={ placeholder } { ...attrs } />
-					:
-					<input className={ this.classNameInput() } type={ type } placeholder={ placeholder } { ...attrs } />
-				}
+				<label>
+					{ !!children &&
+						<p className={ this.classNameCaption() }> { children } </p>
+					}
+
+					{ textarea ?
+						<textarea
+							className={ this.classNameInput() }
+							required={ required }
+							disabled={ disabled }
+							placeholder={ placeholder }
+							{ ...attrs }
+						/>
+						:
+						<input
+							className={ this.classNameInput() }
+							required={ required }
+							disabled={ disabled }
+							type={ type }
+							placeholder={ placeholder }
+							{ ...attrs }
+						/>
+					}
+
+					{ !!message &&
+						<p className={ this.classNameMessage() }> { message } </p>
+					}
+				</label>
 			</div>
 		)
 	}
 }
 
 UiInput.propTypes = {
+	children: PropTypes.node,
 	className: PropTypes.string,
+	required: PropTypes.bool,
+	disabled: PropTypes.bool,
+	error: PropTypes.bool,
+	success: PropTypes.bool,
 	textarea: PropTypes.bool,
 	placeholder: PropTypes.string,
+	message: PropTypes.string,
 
 	type: PropTypes.oneOf( [
 		'text',
