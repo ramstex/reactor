bcdf47a3f841bfd194d0079a62bfc8f4c6fa1adf
r.polunin@buro247.ru <es54P25eseg74>
2022-06-27 16:39:54 +0300

    Fixes

--- src/components/Base/Link/Link.jsx
+++ src/components/Base/Link/Link.jsx
@@ -7,47 +7,63 @@ import { isAbsoluteUrl } from '../../../plugins/helpers';
 class BaseLink extends React.Component {
 	//	Классы
 	classNameRoot() {
-		const { className } = this.props;
-		return classnames('base-link', className);
+		const {
+			className,
+			href,
+		} = this.props;
+
+		return classnames(
+			'base-link',
+			{ '_href': !!href },
+			{ '_no-href': !href },
+			className
+		);
 	}
 
 	isExternal() {
 		const { href } = this.props;
-		return !!href && isAbsoluteUrl(href);
+
+		return !!href && isAbsoluteUrl( href );
 	}
 
 	isInternal() {
 		const { href } = this.props;
-		return !!href && !isAbsoluteUrl(href);
+
+		return !!href && !isAbsoluteUrl( href );
 	}
 
 	render() {
-		const { className, children, href, ...attrs } = this.props;
+		const {
+			className, children, tag: TagName, href, ...attrs
+		} = this.props;
 
-		if (this.isInternal()) {
+		if ( this.isInternal() ) {
 			return (
-				<Link className={this.classNameRoot()} to={href} {...attrs}>
+				<Link className={ this.classNameRoot() } to={ href } { ...attrs }>
 					{children}
 				</Link>
 			);
 		}
 
-		if (this.isExternal()) {
+		if ( this.isExternal() ) {
 			return (
-				<a className={this.classNameRoot()} href={href} {...attrs}>
+				<a className={ this.classNameRoot() } href={ href } { ...attrs }>
 					{children}
 				</a>
 			);
 		}
 
-		return <div {...attrs}>{children}</div>;
+		return <TagName { ...attrs }>{children}</TagName>;
 	}
 }
 
 BaseLink.propTypes = {
 	href: PropTypes.string,
+	tag: PropTypes.string,
 	className: PropTypes.string,
 	children: PropTypes.node,
 };
+
+BaseLink.defaultProps = { tag: 'span' };
 
 export default BaseLink;
--- src/pages/UiKit/pages/Typography/Typography.jsx
+++ src/pages/UiKit/pages/Typography/Typography.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import BaseLink from '../../../../components/Base/Link/Link.jsx';
 import MarkupSection from '../../../../components/Markup/Section/Section.jsx';
 import LocalNav from '../../components/Nav/Nav.jsx';
 
@@ -9,13 +10,14 @@ class PageUiKitTypography extends React.Component {
 	//	Классы
 	classNameRoot() {
 		const { className } = this.props;
-		return classnames('page page-ui-kit-typography', className);
+
+		return classnames( 'page page-ui-kit-typography', className );
 	}
 
 	render() {
 		return (
-			<div className={this.classNameRoot()}>
-				<MarkupSection title={'Ui Kit - Typography'}>
+			<div className={ this.classNameRoot() }>
+				<MarkupSection title={ 'Ui Kit - Typography' }>
 					<LocalNav />
 
 					<h1>Заголовок 1 уровня</h1>
@@ -26,7 +28,7 @@ class PageUiKitTypography extends React.Component {
 					<h6>Заголовок 6 уровня</h6>
 
 					<p>
-						Задача организации, в особенности же постоянный количественный рост и сфера нашей активности
+						Задача <BaseLink href={ 'https://google.com' } target={ '_blank' }>организации</BaseLink>, в особенности же постоянный количественный рост и сфера нашей активности
 						обеспечивает широкому кругу (специалистов) участие в формировании позиций, занимаемых
 						участниками в отношении поставленных задач. Значимость этих проблем настолько очевидна, что
 						укрепление и развитие структуры позволяет оценить значение модели развития. Товарищи! новая
@@ -37,14 +39,14 @@ class PageUiKitTypography extends React.Component {
 						развития.
 					</p>
 
-					<p className={'_sm'}>
+					<p className={ '_sm' }>
 						Товарищи! реализация намеченных плановых заданий позволяет оценить значение направлений
 						прогрессивного развития. Таким образом сложившаяся структура организации способствует подготовки
 						и реализации форм развития. Равным образом дальнейшее развитие различных форм деятельности
 						играет важную роль в формировании соответствующий условий активизации.
 					</p>
 
-					<p className={'_lg'}>
+					<p className={ '_lg' }>
 						Равным образом консультация с широким активом влечет за собой процесс внедрения и модернизации
 						соответствующий условий активизации. Таким образом новая модель организационной деятельности
 						требуют определения и уточнения дальнейших направлений развития. Разнообразный и богатый опыт
@@ -52,7 +54,7 @@ class PageUiKitTypography extends React.Component {
 						обучения кадров, соответствует насущным потребностям.
 					</p>
 
-					<p className={'_footnote'}>
+					<p className={ '_footnote' }>
 						Задача организации, в особенности же сложившаяся структура организации играет важную роль в
 						формировании позиций, занимаемых участниками в отношении поставленных задач. Товарищи! новая
 						модель организационной деятельности требуют определения и уточнения соответствующий условий
@@ -71,8 +73,6 @@ class PageUiKitTypography extends React.Component {
 	}
 }
 
-PageUiKitTypography.propTypes = {
-	className: PropTypes.node,
-};
+PageUiKitTypography.propTypes = { className: PropTypes.node };
 
 export default PageUiKitTypography;
