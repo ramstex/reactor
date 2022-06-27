20c573d8da93c24f6f876a4773a661bc108b9df7
r.polunin@buro247.ru <es54P25eseg74>
2022-06-27 22:41:50 +0300

    Fixes

--- src/pages/UiKit/pages/Inputs/Inputs.jsx
+++ src/pages/UiKit/pages/Inputs/Inputs.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
+import React, { useState } from 'react';
+import omit from 'lodash/omit.js';
 
 import MarkupSection from '../../../../components/Markup/Section/Section.jsx';
-import { Row, Col } from '../../../../components/Grid/Grid.jsx';
+import {
+	Row, Col
+} from '../../../../components/Grid/Grid.jsx';
 import UiInput from '../../../../components/Ui/Input/Input.jsx';
 import LocalNav from '../../components/Nav/Nav.jsx';
 
@@ -11,44 +12,31 @@ import './Inputs.scss';
 
 import { createDummyState } from './dummy.jsx';
 
-class PageUiKitInputs extends React.Component {
-	constructor( props ) {
-		super( props );
-		this.state = createDummyState.call( this );
-	}
+const PageUiKitInputs = () => {
+	const [ inputs, setInputs ] = useState( createDummyState().inputs );
 
-	//	Классы
-	classNameRoot() {
-		const { className } = this.props;
+	return (
+		<div className={ 'page page-ui-kit-inputs' }>
+			<MarkupSection title={ 'Ui Kit - Inputs' }>
+				<LocalNav />
 
-		return classnames( 'page page-ui-kit-inputs', className );
-	}
-
-	render() {
-		const { inputs } = this.state;
-
-		return (
-			<div className={ this.classNameRoot() }>
-				<MarkupSection title={ 'Ui Kit - Inputs' }>
-					<LocalNav />
-
-					<div>
-						{ inputs.map( ( item, index ) => {
-							return (
-								<Row className={ '_mb_4' } key={ index }>
-									<Col col={ 4 }>
-										<UiInput { ...item.props } > { item.label } </UiInput>
-									</Col>
-								</Row>
-							);
-						} ) }
-					</div>
-				</MarkupSection>
-			</div>
-		);
-	}
-}
-
-PageUiKitInputs.propTypes = { className: PropTypes.string };
+				<div>
+					{ inputs.map( ( item, index ) => {
+						return (
+							<Row className={ '_mb_4' } key={ index }>
+								<Col col={ 4 }>
+									<UiInput { ...item.props } onChange={ ( event ) => {
+										item.props.value = event.target.value;
+										setInputs( [ ...inputs ] );
+									} }> { item.label } </UiInput>
+								</Col>
+							</Row>
+						);
+					} ) }
+				</div>
+			</MarkupSection>
+		</div>
+	);
+};
 
 export default PageUiKitInputs;
--- src/pages/UiKit/pages/Inputs/dummy.jsx
+++ src/pages/UiKit/pages/Inputs/dummy.jsx
@@ -1,20 +1,18 @@
-const onChange = ( context, event, index, props ) => {
-	const { inputs } = context.state;
-	inputs[ index ] = {
-		...inputs[ index ],
-		props: {
-			...inputs[ index ].props,
-			...props,
-			value: event.target.value,
-		},
-	};
+// import React, { useState } from 'react';
 
-	context.setState( { inputs } );
-};
+// const onChange = ( input, event, props ) => {
+// 	// const { inputs } = state;
+// 	input = {
+// 		...input,
+// 		props: {
+// 			...input.props,
+// 			...props,
+// 			value: event.target.value,
+// 		},
+// 	};
+// };
 
 export function createDummyState() {
-	const context = this;
-
 	return {
 		inputs: [
 			{
@@ -23,9 +21,6 @@ export function createDummyState() {
 				props: {
 					value: '',
 					type: 'email',
-					onChange( event ) {
-						onChange( context, event, 0 );
-					},
 					onValid( event ) {
 						console.log( 'VALID', event );
 					},
@@ -34,90 +29,90 @@ export function createDummyState() {
 					},
 				},
 			},
-
-			{
-				label: 'Текстовое поле',
-
-				props: {
-					value: 1234567890,
-					textarea: true,
-					onChange( event ) {
-						onChange( context, event, 1 );
-					},
-				},
-			},
-
-			{
-				label: 'Инпут с плейсхолдером',
-
-				props: {
-					value: '',
-					placeholder: 'placeholder',
-					required: true,
-					onChange( event ) {
-						onChange( context, event, 2 );
-					},
-				},
-			},
-
-			{
-				label: 'Поле read only',
-
-				props: {
-					value: 'Read Only',
-					readOnly: true,
-					onChange( event ) {
-						onChange( context, event, 3 );
-					},
-				},
-			},
-
-			{
-				label: 'Неактивное поле',
-
-				props: {
-					value: 'Disabled',
-					disabled: true,
-					onChange( event ) {
-						onChange( context, event, 4 );
-					},
-				},
-			},
-
-			{
-				label: 'Успешное поле',
-
-				props: {
-					value: 'Success',
-					success: true,
-					message: 'Success message',
-					onChange( event ) {
-						onChange( context, event, 5, {
-							error: false,
-							success: false,
-							message: '',
-						} );
-					},
-				},
-			},
-
-			{
-				label: 'Ошибочное поле',
-
-				props: {
-					value: 'Error',
-					error: true,
-					success: true,
-					message: 'Error message',
-					onChange( event ) {
-						onChange( context, event, 6, {
-							error: false,
-							success: false,
-							message: '',
-						} );
-					},
-				},
-			},
+			//
+			// {
+			// 	label: 'Текстовое поле',
+			//
+			// 	props: {
+			// 		value: 1234567890,
+			// 		textarea: true,
+			// 		onChange( event ) {
+			// 			onChange( event, 1 );
+			// 		},
+			// 	},
+			// },
+			//
+			// {
+			// 	label: 'Инпут с плейсхолдером',
+			//
+			// 	props: {
+			// 		value: '',
+			// 		placeholder: 'placeholder',
+			// 		required: true,
+			// 		onChange( event ) {
+			// 			onChange( event, 2 );
+			// 		},
+			// 	},
+			// },
+			//
+			// {
+			// 	label: 'Поле read only',
+			//
+			// 	props: {
+			// 		value: 'Read Only',
+			// 		readOnly: true,
+			// 		onChange( event ) {
+			// 			onChange( event, 3 );
+			// 		},
+			// 	},
+			// },
+			//
+			// {
+			// 	label: 'Неактивное поле',
+			//
+			// 	props: {
+			// 		value: 'Disabled',
+			// 		disabled: true,
+			// 		onChange( event ) {
+			// 			onChange( event, 4 );
+			// 		},
+			// 	},
+			// },
+			//
+			// {
+			// 	label: 'Успешное поле',
+			//
+			// 	props: {
+			// 		value: 'Success',
+			// 		success: true,
+			// 		message: 'Success message',
+			// 		onChange( event ) {
+			// 			onChange( event, 5, {
+			// 				error: false,
+			// 				success: false,
+			// 				message: '',
+			// 			} );
+			// 		},
+			// 	},
+			// },
+			//
+			// {
+			// 	label: 'Ошибочное поле',
+			//
+			// 	props: {
+			// 		value: 'Error',
+			// 		error: true,
+			// 		success: true,
+			// 		message: 'Error message',
+			// 		onChange( event ) {
+			// 			onChange( event, 6, {
+			// 				error: false,
+			// 				success: false,
+			// 				message: '',
+			// 			} );
+			// 		},
+			// 	},
+			// },
 		],
 	};
 }
