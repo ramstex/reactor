be3adb2a96db55ffe1ccd0886e59f0c235b32969
roman <polunin@alkor.co.ru>
2022-07-12 18:58:17 +0300

    Рефакторинг

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import store from './store/index.js';
-import { Provider } from 'react-redux';
 
 import App from './components/App/App.jsx';
 
 import './style.scss';
 
-ReactDOMClient.createRoot(document.getElementById('app-root')).render(
-	<Provider store={store}>
+ReactDOMClient.createRoot( document.getElementById( 'app-root' ) )
+	.render(
 		<App />
-	</Provider>
-);
+	);
